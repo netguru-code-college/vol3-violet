2a0b95b412271694678c80855b79f0209c24bd5c
TomKo1 <kottomasz98@gmail.com>
2018-08-24 12:52:02 +0200

    Added carriewave for avatar to project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, length: { in: 2..20 }
